5c117bb80e5bfe4b2a4bd5760052b9d5f122a8c8
nslo <nathan.slobody@gmail.com>
2014-12-27 14:31:16 -0600

    fish prompt tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,24 @@
+set EDITOR vim
+set BROWSER firefox
+set PACMAN pacmatic
+
+set -x PATH $PATH .cabal/bin:$PATH
+
 set -g fish_key_bindings fish_vi_key_bindings
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+set -g __fish_git_prompt_char_stagedstate '→'
+set -g __fish_git_prompt_char_dirtystate "+"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "x"
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,59 +5,45 @@ function fish_prompt --description 'Write out the prompt'
 function fish_vi_prompt_cm --description "Displays the current mode"
   switch $fish_bind_mode
     case default
-      set_color --bold --background red white
+      #set_color --bold --background red white
+      set_color --bold red
       echo "[N]"
     case insert
-      set_color --bold --background green white
+      #set_color --bold --background green white
+      set_color --bold 888
       echo "[I]"
     case visual
-      set_color --bold --background magenta white
+      #set_color --bold --background magenta white
+      set_color --bold magenta
       echo "[V]"
   end
   set_color normal
 end
 
-# Just calculate these once, to save a few cycles when displaying the prompt
-    if not set -q __fish_prompt_hostname
-        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-    end
-
-if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
-
-if not set -q __fish_color_blue
-        set -g __fish_color_blue (set_color -o blue)
-    end
-
-#Set the color for the status depending on the value
-    set __fish_color_status (set_color -o green)
-    if test $stat -gt 0
-        set __fish_color_status (set_color -o red)
-    end
-
-switch $USER
-
-case root
-
-if not set -q __fish_prompt_cwd
-            if set -q fish_color_cwd_root
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-            else
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-        end
-
-printf '%s@%s %s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
-case '*'
-
-if not set -q __fish_prompt_cwd
-            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-        end
-
-printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r' (date "+%y/%m/%d %H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (pwd) "$__fish_color_status" "$stat" "$__fish_prompt_normal" 
-echo -n -s (fish_vi_prompt_cm) ' $ '
-
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+    printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
 end
+
+# Just calculate these once, to save a few cycles when displaying the prompt
+if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+end
+if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+end
+
+# Draw the prompt.
+set_color yellow
+echo -n -s '['(date "+%y/%m/%d %H:%M:%S")'] '
+set_color -o blue
+echo -n -s $USER '@' $__fish_prompt_hostname ' '
+set_color -o green
+echo -n -s (prompt_pwd) ' '
+set_color 888
+echo -n -s '(' $stat ') '
+echo -n -s $__fish_prompt_normal
+echo -n -s (__fish_git_prompt)
+printf '\f\r'
+echo -n -s (fish_vi_prompt_cm) ' '
+
 end
